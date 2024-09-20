33da06c802bf047f5fc98097e266e35d85747a77
henrry <henrryademarmamani@gmail.com>
2024-09-20 19:09:59 -0400

    feature(common): [added Question-2]

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import { List } from './components/List'
 import { WrapperList } from './components/WrapperList'
+import { Button } from './components/button'
 function App() {
   return (
     <>
       <WrapperList>
           <List/>
+          <Button/>
       </WrapperList>
     </>
   )
