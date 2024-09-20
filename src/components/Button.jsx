2ae646a122a252484e46564eaed73c89ae758df6
henrry <henrryademarmamani@gmail.com>
2024-09-20 19:22:58 -0400

    feature(common): [added Question-3]

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,16 +1,18 @@
 import { useState } from "react";
-export const Button = props => {
 
+export const Button = (props) => {
   const { text } = props;
+  const [count, setCount] = useState(0);
 
-  const[count, setCount] = useState(0);
-  
   function handlerButton() {
+    const randomValue = Math.floor(Math.random() * 100) + 1; 
+    alert(randomValue);
     setCount(count + 1);
   }
+
   return (
-    <button onClick={ handlerButton}>
-        { text } { count }
+    <button onClick={handlerButton}>
+      {text} {count}
     </button>
   );
-}
+};
